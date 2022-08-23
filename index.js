af3aabbdcf1cbb546e2ee2443f5822e93d12c0b1
Davi Serrano <daviscardoso2901@gmail.com>
2022-08-23 20:40:34 -0300

    trabalhando nos links

---
+++ index.js
@@ -0,0 +1,7 @@
+const BntTelgram = document.getElementById("telegram")
+const btnYoutube = document.getElementById("youtube")
+const btnInstagram = document.getElementById("instagra,")
+
+BntTelgram.addEventListener("click", ()=> location.href = "https://t.me/thaaay_niely" )
+btnYoutube.addEventListener("click", ()=> location.href = "https://www.youtube.com/thayniely" )
+btnInstagram.addEventListener("click", ()=> location.href = "https://www.instagram.com/thaaay_niely/" )
