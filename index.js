459d300e78817819cb5f6ba2a395b6855ea9b1e0
Davi Serrano <daviscardoso2901@gmail.com>
2022-09-01 12:46:48 -0300

    arrumando imagem e perfil

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const BntTelgram = document.getElementById("telegram")
 const btnYoutube = document.getElementById("youtube")
-const btnInstagram = document.getElementById("instagra,")
+const btnInstagram = document.getElementById("instagram")
 
 BntTelgram.addEventListener("click", ()=> location.href = "https://t.me/thaaay_niely" )
 btnYoutube.addEventListener("click", ()=> location.href = "https://www.youtube.com/thayniely" )
